932536410c06e24880e7e3416ea84d3c00b45863
AvisheikhKundu <avisheikh76@gmail.com>
2023-02-05 19:36:13 +0600

    code add

--- test.c
+++ test.c
@@ -1,4 +1,14 @@
 #include<stdio.h>
-int main(
-    
-)
+#include<stdlib.h>
+#include<string.h>
+
+struct Sales_man
+{
+    int id;
+    char name[50];
+    char phone[20];
+    int hour;
+    struct Sales_man *next;
+
+}* head;
+void insert(int id, char* name, char* phone, int hour)
